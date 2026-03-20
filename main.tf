e0061bb0e560c9d6a3e2f4de0065e14016932ea4
dm <pomogoss@yandex.ru>
2026-03-19 20:20:26 -0700

    update main.tf

--- main.tf
+++ main.tf
@@ -3,13 +3,15 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami                         = "ami-02dfbd4ff395f2a1b"
-  instance_type               = "t2.micro"
-  user_data                   = <<-EOF
-                                #!/bin/bash
-                                echo "Hello, World" > index.html
-                                nohup busybox httpd -f -p 8080 &
-                                EOF
+  ami                    = "ami-02dfbd4ff395f2a1b"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data              = <<-EOF
+#!/bin/bash
+echo "Hello, World" > index.html
+nohup busybox httpd -f -p 8080 &
+EOF
+
   user_data_replace_on_change = true
 
   tags = {
@@ -20,10 +22,10 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
-  ingress = {
-    from_port  = 8080
-    to_port    = 8080
-    protocol   = "tcp"
-    cidr_block = ["0.0.0.0/0"]
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
